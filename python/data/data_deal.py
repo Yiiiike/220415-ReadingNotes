b4881a561b19600a431eeeff08de43fb056cddb6
keke和keke <1042376975@qq.com>
2023-10-06 13:22:44 +0800

    common update

--- python/data/data_deal.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-data = pd.read_excel('data.xlsx')
-
-print(data)
--- python/test_coding/tmp.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('Nowcoder.csv')
-print(data.iloc[-5:][['Nowcoder_ID', 'Level', 'Achievement_value', 'Language']])
